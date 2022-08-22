ea1933139a85c282036f6f758205ca4e50eaae8b
anhkhoale1 <leanhkhoa199937@gmail.com>
2022-08-22 23:35:41 +0200

    test technique reactjs cocktail api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import {parseIngredients, clone, parseDrinks} from './Utils';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [cocktails, setCocktails] = useState("");
+  const [result, setResult] = useState("");
+  const [cocktailSelected, setCocktailsSelected] = useState([]);
+  const [cocktailsOnList, setCocktailsOnList] = useState("");
+  
+  const changeHandler = (e) => {
+    const letter = e.target.value;
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + letter;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => setCocktails(data));
+  }
+  
+  const addOnList = (e) => {
+    e.preventDefault();
+    const cocktail_id = e.target.attributes.getNamedItem('cocktail-id').value;
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + cocktail_id;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => (data.drinks != null && data.length !== 0) ? setCocktailsSelected(current => [...current, data.drinks[0]]) : null);
+  }
+  
+  const removeFromList = (e) => {
+    const cocktailId = e.target.getAttribute('cocktail_id');
+    if (!cocktailId) {
+      return;
+    }
+    const foundIndex = cocktailSelected.findIndex((cocktail) => cocktail.idDrink === cocktailId);
+    if (foundIndex === -1) {
+      return;
+    }
 
-export default App;
+    var clonedCocktailSelected = clone(cocktailSelected);
+    clonedCocktailSelected.splice(foundIndex, 1);
+    setCocktailsSelected(clonedCocktailSelected);
+  }
+  
+  useEffect(() => {
+    if (cocktailSelected) {
+      showCocktails();
+    }
+  }, [cocktailSelected]);
+  
+  const showCocktails = () => {
+    const data = parseDrinks(cocktailSelected);
+    if(data) {
+      // let accumulatedCocktail = retriveAccumulatedCocktails(data);
+      let cocktailsOnList = data.map((cocktail) => (
+        <div className="main-list" key={cocktail.idDrink}>
+        <h2 className='main-title'><b>{cocktail.name}</b> ({cocktail.cpt}) </h2>
+        {cocktail.ingredients.map((ingredient) => (
+          <h2 className='main-ingredient'>{ingredient.name} - {ingredient.quantity} {ingredient.unity} </h2>
+        ))}
+        <button className='remove-btn' cocktail_id={cocktail.idDrink} onClick={removeFromList}>Remove</button>
+        </div>
+        ));
+        setCocktailsOnList(cocktailsOnList);
+      }
+    }
+    
+    const showResult = (e) => {
+      e.preventDefault();
+      const data = cocktails.drinks;
+      if(data) {
+        let listCocktails = data.map((cocktail) => (
+          <div className="main-card" key={cocktail.idDrink}>
+          <img
+          src={cocktail.strDrinkThumb}
+          alt='cocktails'
+          height='200'
+          width='200'
+          />
+          <h2 className='main-title'>{cocktail.strDrink}</h2>
+          <button className='add-btn' cocktail-id={cocktail.idDrink} onClick={addOnList}>Add</button>
+          </div>
+          ));
+          setResult(listCocktails);
+        }
+      }
+      
+      return (
+        <>
+        <div className='main-container'>
+        <form onSubmit={showResult}>
+        <div className='search-container'>
+        <input type='search' className='search-field' onChange={changeHandler}/><br/>
+        <button type='submit' className='submit-btn'> Search Cocktails</button>
+        </div>
+        </form>
+        </div>
+        <div>{result}</div>
+        <div>{cocktailsOnList}</div>
+        </>
+        );
+      }
+      
+      export default App;
+      
---
+++ src/Utils.js
@@ -0,0 +1,108 @@
+
+
+const MAX_LENGTH = 15; 
+const PREFIX_INGREDIENT = 'strIngredient';
+const PREFIX_MEASURE = 'strMeasure';
+
+export const parseDrinks = (drinks) => {
+  var cocktails = [];
+  for (const apiCocktail of drinks) {
+    var cocktail = {idDrink: "", name: "", ingredients: [], cpt: 0};
+
+    const foundIndex = cocktails.findIndex((cocktail) => cocktail.idDrink === apiCocktail.idDrink);
+    if (foundIndex === -1) {
+      cocktail.idDrink = apiCocktail.idDrink;
+      cocktail.name = apiCocktail.strDrink;
+      cocktail.ingredients = parseIngredients(apiCocktail);
+      cocktail.cpt = 1;
+      cocktails.push(cocktail);
+    }
+    else {
+      const newIngredients = parseIngredients(apiCocktail);
+      cocktails[foundIndex].ingredients = addIngredients(cocktails[foundIndex].ingredients, newIngredients);
+      cocktails[foundIndex].cpt += 1;
+    }
+  }
+  console.log(cocktails);
+  return cocktails;
+}
+
+function isNumber(str) {
+  try {
+      eval(str);
+  } catch (err){
+      return false;
+  }
+  return true;
+}
+
+function addIngredients(existingIngredients, newIngredients) {
+  // we assume that existing new ingredients and existing ingredients have the same component
+  var resIngredients = [];
+  existingIngredients.forEach((element, index) => {
+    resIngredients.push(
+      {
+        name: element.name, 
+        quantity: element.quantity + newIngredients[index].quantity, 
+        unity: element.unity
+      });    
+  });
+  return resIngredients;
+}
+
+function parseMeasureQuantity(strMeasure) {
+  var res = {measure:"", quantity:0};
+  if (strMeasure == null)
+      return res;
+
+  var measure = strMeasure.trim(); 
+  var splittedMeasure = measure.split(" ");
+
+  if (splittedMeasure.length === 0)
+      return res;
+
+  var m = splittedMeasure[splittedMeasure.length - 1];
+  while(splittedMeasure.length != 0 && !isNumber(m))
+  {
+      splittedMeasure.pop();
+      res.measure = m + " " + res.measure;
+      m = splittedMeasure[splittedMeasure.length - 1]; 
+  }
+
+  res.quantity = splittedMeasure.reduce((acc, val) => isNumber(val) ? eval(acc) + eval(val) : eval(acc), 0);
+  res.measure = res.measure.trim();
+  return res;
+}
+
+export const parseIngredients = (drink) => {
+  let ingredients = [];
+  for (let index = 1; index <= MAX_LENGTH; index++) {
+    const strIngredient = PREFIX_INGREDIENT + index;
+    const strMeasure = PREFIX_MEASURE + index;
+    let ingredient = {name: "", quantity: 0, unity: ""};
+    if (drink[strIngredient] != null) {
+      ingredient.name = drink[strIngredient];
+    }
+    if (drink[strMeasure] != null) {
+      var parsedMeasure = parseMeasureQuantity(drink[strMeasure]);
+      ingredient.quantity = parsedMeasure.quantity;
+      ingredient.unity = parsedMeasure.measure;
+    }
+    if (ingredient.name.length !== 0) {
+      ingredients.push(ingredient);
+    }
+  }
+
+  return ingredients;
+}
+
+export const clone = (data) => {
+  return JSON.parse(JSON.stringify(data));
+}
+
+// [{ name: Yogurt Cooler, 
+//   ingredients:[ {name : "Tequila",  measure: "3 oz" } , { name: ..., measure:...} ,...]
+// }, 
+// { name : Yellow Bird, 
+// ingredients : [...]
+// }]
